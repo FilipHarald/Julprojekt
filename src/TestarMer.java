f56fc88fa1cda165739be618beacdfb8868e578b
FilipHarald <filip.harald@gmail.com>
2014-12-17 09:18:12 +0100

    Tog bort testerna

--- src/TestarMer.java
+++
@@ -1,9 +0,0 @@
-
-public class TestarMer {
-	public static void main(String[] args){
-		System.out.print("hej");
-		System.out.print("hej");
-		System.out.println();
-		System.out.println("Andreas älskar att sova");
-	}
-}
