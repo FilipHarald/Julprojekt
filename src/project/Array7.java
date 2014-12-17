300e708150aeefd309e4d66c67d3a1e96ce6ba12
Davids <davve52@hotmail.com>
2014-12-17 10:44:24 +0100

    Uppgift 3.2.2:

--- src/project/Array7.java
+++ src/project/Array7.java
@@ -1,17 +1,21 @@
 package project;
 
 public class Array7 {
+	private int [] arr7;
 	
-	
-	private int [] arr7 = new int[7];
-	
-	public void setElement(int row, int value){
-		arr7[row] = value;
+	public Array7(){
+		arr7 = new int[7];
 		
+	}
+	
+	public void setElement(int pos, int value){
+		arr7[pos] = value;
 	}
 	
 	public int getElement(int row){
 		return arr7[row];
 	}
+	
+	
 
 }
--- src/project/Array7x7.java
+++ src/project/Array7x7.java
@@ -1,13 +1,13 @@
 package project;
 
 public class Array7x7 {
-	
-	private int row;
-	private int col;
-	private int[][] arr7x7 = new int[7][7];
+	private int[][] arr7x7;
 	
 	
-	
+	public Array7x7(){
+		arr7x7 = new int[7][7];
+		
+	}
 	
 	public void setElement(int row, int col, int value){
 		arr7x7[row][col] = value;
@@ -21,5 +21,31 @@ public class Array7x7 {
 		
 	}
 	
-
+	public void setRow(int row, Array7 theRow){ // vi har tagit bort int col!
+		for(int i = 0; i < 7; i++){
+			arr7x7[row][i] = theRow.getElement(i);			
+		}
+	}
+	
+	public Array7 getRow(int row){
+		Array7 temp = new Array7();
+		for(int i = 0; i < 7; i++){
+			temp.setElement(i, arr7x7[row][i]);
+		}
+		return temp;
+	}
+	
+	public void setCol(int col, Array7 theCol){	// vi har tagit bort int row
+		for(int i = 0; i < 7; i++){
+			arr7x7[i][col] = theCol.getElement(i);
+		}
+	}
+	
+	public Array7 getCol(int col){
+		Array7 temp = new Array7();
+		for(int i = 0; i < 7; i++){
+			temp.setElement(i, arr7x7[i][col]);
+		}
+		return temp;
+	}
 }
