730e8ba423572a32b067bf2d009dd6172d20462e
Davids <davve52@hotmail.com>
2014-12-17 09:52:47 +0100

    Uppgift 321

---
+++ src/project/Array7.java
@@ -0,0 +1,17 @@
+package project;
+
+public class Array7 {
+	
+	
+	private int [] arr7 = new int[7];
+	
+	public void setElement(int row, int value){
+		arr7[row] = value;
+		
+	}
+	
+	public int getElement(int row){
+		return arr7[row];
+	}
+
+}
