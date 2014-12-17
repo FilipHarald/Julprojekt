92db681972f923253e4cb8dc4c4f7e9e65a74c12
Joakim1331 <joakim_naw@hotmail.com>
2014-12-17 09:00:57 +0100

    test2

--- src/TestarMer.java
+++ src/TestarMer.java
@@ -3,5 +3,7 @@ public class TestarMer {
 	public static void main(String[] args){
 		System.out.print("hej");
 		System.out.print("hej");
+		System.out.println();
+		System.out.println("Andreas älskar att sova");
 	}
 }
