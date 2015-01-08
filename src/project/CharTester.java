6bdf2651e43aa073bc0abf906b44d832a170ed03
appelqvist <andreas.appelqvist@gmail.com>
2015-01-08 11:32:35 +0100

    CharTester går att mata in A,B,C,D,E,F

--- src/project/CharTester.java
+++ src/project/CharTester.java
@@ -4,42 +4,64 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class CharTester extends JFrame{
-	
-	public CharTester(){
+public class CharTester extends JFrame {
+//	private Char cLibrary;
+	private int[][] chrArray;
+
+	public CharTester(int[][] chrArray) {
+		this.chrArray = chrArray;
+
 		setTitle("CharTester");
 		setSize(360, 380);
 		setBackground(java.awt.Color.WHITE);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
-	public void paint(Graphics g) {	
-		
-		int[][] arr = {
-				{0,0,1,1,0,0,0},
-				{0,1,0,0,1,0,0},
-				{0,1,0,0,1,0,0},
-				{0,1,1,1,1,0,0},
-				{0,1,0,0,1,0,0},
-				{0,1,0,0,1,0,0},
-				{0,1,0,0,1,0,0}};
-		
-		for (int y = 0; y < 7; y ++) {
-			for (int x = 0; x < 7; x ++) {
-				if(arr[y][x] == 1){
+
+	public void paint(Graphics g) {
+
+		for (int y = 0; y < 7; y++) {
+			for (int x = 0; x < 7; x++) {
+				if (this.chrArray[y][x] == 1) {
 					g.setColor(Color.BLACK);
-				}
-				else{
+				} else {
 					g.setColor(Color.LIGHT_GRAY);
 				}
-				g.fillRect((x*50+10), (y*50+30), 40, 40);
+				g.fillRect((x * 50 + 10), (y * 50 + 30), 40, 40);
 			}
 		}
 	}
-	
+
 	public static void main(String args[]) {
-		CharTester tester = new CharTester();
+		Char cLibrary = new Char();  
+		String c = JOptionPane.showInputDialog("Mata in ett tecken");
+		c = c.toUpperCase();
+		char chr = c.charAt(0);
+
+		
+		int[][] arr = new int[7][7];
+
+		if (chr == 'A') {
+			arr = cLibrary.charA; 
+		}
+		if (chr == 'B') {
+			arr = cLibrary.charB;
+		}
+		if (chr == 'C') {
+			arr = cLibrary.charC;
+		}
+		if (chr == 'D') {
+			arr = cLibrary.charD;
+		}
+		if (chr == 'E') {
+			arr = cLibrary.charE;
+		}
+		if (chr == 'F') {
+			arr = cLibrary.charF;
+		}
+
+		CharTester tester = new CharTester(arr);
 	}
 }
